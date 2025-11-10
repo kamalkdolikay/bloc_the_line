28539c14e0ad135f9d9fe869b484a9db0df41bd5
Travis Gooden <tgooden@my.bcit.ca>
2025-11-09 18:56:21 -0800

    chore: add shapes above pieces as comments

--- lib/bloc_the_line/game/piece.ex
+++ lib/bloc_the_line/game/piece.ex
@@ -8,53 +8,110 @@ defmodule Piece do
 end
 
 defmodule Pieces do
+  # all pieces are "anchored" to the top left corner
   @pieces %{
 
     # F
+
+    # █
+    #███
+    #█ 
     "F" => %Piece{},
 
     # I pieces
+
+    #█
     "1" => %Piece{},
+    #██
     "2" => %Piece{},
+    #███
     "I3" => %Piece{},
+    #████
     "I4" => %Piece{},
+    #█████     
     "I5" => %Piece{},
 
     # L pieces
+
+    #█
+    #███
     "L4" => %Piece{},
+    #████     
+    #█
     "L5" => %Piece{},
 
     # N
+    # ███
+    #██
     "N" => %Piece{},
 
     # O
+
+    #██
+    #██
     "O" => %Piece{},
 
     # P
+
+    #██
+    #██
+    #█
     "P" => %Piece{},
 
     # T
+
+    # █
+    #███
     "T4" => %Piece{},
+    # █
+    # █
+    #███
     "T5" => %Piece{},
 
     # U
+
+    #███
+    #█ █
     "U" => %Piece{},
 
     # V
+
+    #██
+    # █
     "V3" => %Piece{},
+    #█
+    #█
+    #███
     "V5" => %Piece{},
 
     # W
+
+    #█
+    #██
+    # ██
     "W" => %Piece{},
 
     # X
+
+    # █
+    #███
+    # █
     "X" => %Piece{},
 
     # Y
-    "X" => %Piece{},
+    #████     
+    # █     
+    "Y" => %Piece{},
 
     # Z
+
+    #█
+    #██
+    # █
     "Z4" => %Piece{},
+    #█
+    #███
+    #  █
     "Z5" => %Piece{},
   }
 end
