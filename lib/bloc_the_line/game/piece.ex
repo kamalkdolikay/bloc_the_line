5db74508634a88b3f01803ae8aa9d6962c88518b
Travis Gooden <tgooden@my.bcit.ca>
2025-11-09 18:14:59 -0800

    feat: add stubs for all pieces

--- lib/bloc_the_line/game/piece.ex
+++ lib/bloc_the_line/game/piece.ex
@@ -1,6 +1,60 @@
 defmodule Piece do
+  alias Piece # avoids having to use bloc_the_line.game.Piece
   defstruct name: nil, cells: [], corners: []
 
   @type coordinate :: {integer(), integer()}
   @type t :: %__MODULE__{name: String.t(), cells: MapSet.t(coordinate()), corners: MapSet.t(coordinate())}
+
+end
+
+defmodule Pieces do
+  @pieces %{
+
+    # F
+    "F" => %Piece{},
+
+    # I pieces
+    "1" => %Piece{},
+    "2" => %Piece{},
+    "I3" => %Piece{},
+    "I4" => %Piece{},
+    "I5" => %Piece{},
+
+    # L pieces
+    "L4" => %Piece{},
+    "L5" => %Piece{},
+
+    # N
+    "N" => %Piece{},
+
+    # O
+    "O" => %Piece{},
+
+    # P
+    "P" => %Piece{},
+
+    # T
+    "T4" => %Piece{},
+    "T5" => %Piece{},
+
+    # U
+    "U" => %Piece{},
+
+    # V
+    "V3" => %Piece{},
+    "V5" => %Piece{},
+
+    # W
+    "W" => %Piece{},
+
+    # X
+    "X" => %Piece{},
+
+    # Y
+    "X" => %Piece{},
+
+    # Z
+    "Z4" => %Piece{},
+    "Z5" => %Piece{},
+  }
 end
