6fe13cbb637a290d3c15ae99337da59f5f15f750
Anhad Sodhi <anhadsodhi2005@gmail.com>
2025-11-09 22:22:04 -0800

    lib/bloc_the_line_web/components/blokus_board.ex: Added html div as container for pieces

--- lib/bloc_the_line_web/components/blokus_board.ex
+++ lib/bloc_the_line_web/components/blokus_board.ex
@@ -5,16 +5,27 @@ defmodule BlocTheLineWeb.BlokusBoard do
 
   def blokus_board(assigns) do
     ~H"""
-    <div class="inline-block border border-gray-400">
+    <div
+      id="blokus-board"
+      phx-hook="MovingBlock"
+      data-rows={Enum.count(@board)}
+      data-cols={@board |> List.first() |> Enum.count()}
+      class="inline-block border border-gray-400 relative"
+    >
+      <!-- overlay block that will be moved client-side. block-size is in tiles (w x h). -->
+      <div class="moving-block" data-block-w="2" data-block-h="2" data-row="0" data-col="0" aria-hidden="true"></div>
+
       <%= for {row, row_index} <- Enum.with_index(@board) do %>
-        <div class="flex">
+        <div class="flex blokus-row">
           <%= for {cell, col_index} <- Enum.with_index(row) do %>
             <div
               phx-click="cell_click"
               phx-value-row={row_index}
               phx-value-col={col_index}
+              data-row={row_index}
+              data-col={col_index}
               class={[
-                "w-8 h-8 border border-gray-300 cursor-pointer",
+                "blokus-tile w-8 h-8 border border-gray-300 cursor-pointer",
                 cell == 1 && "bg-blue-500",
                 cell == 0 && "bg-white"
               ]}
