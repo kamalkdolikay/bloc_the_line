ceca517f6cb40b7001650560bffbc272d7a59fad
Ben Oh <moh16@my.bcit.ca>
2025-11-14 13:01:06 -0800

    feat: add timer cancel and reset functionality

--- lib/bloc_the_line/game/timer.ex
+++ lib/bloc_the_line/game/timer.ex
@@ -7,4 +7,15 @@ defmodule GameTimer do
     Process.send_after(self(), :game_over_timeout, duration_seconds * 1000)
   end
 
+  def cancel_timer(timer_ref) when is_reference(timer_ref) do
+    Process.cancel_timer(timer_ref)
+  end
+
+  def cancel_timer(nil), do: :ok
+
+  def reset_timer(old_timer_ref, duration_seconds \\ @default_timer_duration_seconds) do
+    cancel_timer(old_timer_ref)
+    start_timer(duration_seconds)
+  end
+
 end
