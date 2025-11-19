78b14f03308bd16181ec9e3d794129bf39955f57
Kamal Dolikay <kamaldolikay@gmail.com>
2025-11-19 00:28:19 -0800

    chore(Format): all elixir files formatted

--- lib/bloc_the_line_web/components/navbar.ex
+++ lib/bloc_the_line_web/components/navbar.ex
@@ -5,7 +5,7 @@ defmodule BlocTheLineWeb.Navbar do
     ~H"""
     <div id="navbar" class="flex justify-between items-center">
       <div><img src="/images/bloc_the_line_logo.png" alt="Logo" class="h-12 w-auto" /></div>
-       <.link navigate="" class="">Lobby</.link>
+      <.link navigate="" class="">Lobby</.link>
     </div>
     """
   end
