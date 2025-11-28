fa47e0a2c4e00ab533c5765b5d52b988f7349abb
MarcusLages <marcusvlages@gmail.com>
2025-11-27 22:33:53 -0800

    Changed Player struct to add extra helper functions and the corner attribute

--- lib/bloc_the_line/game/player.ex
+++ lib/bloc_the_line/game/player.ex
@@ -14,7 +14,7 @@ defmodule Player do
           name: String.t(),
           color: non_neg_integer(), # More like a team or player number, not exactly colour
           points: non_neg_integer(),
-          start_location: coord(),
+          corner: coord(),
           board_location: coord(),
           current_piece: Piece.t() | nil,
           ready: boolean(),
@@ -25,7 +25,7 @@ defmodule Player do
     :id,
     :name,
     :color,
-    :start_location,
+    :corner,
     :board_location,
     :joined_at,
     points: 0,
@@ -33,16 +33,16 @@ defmodule Player do
     ready: false
   ]
 
-  @doc "Create a player; board_location starts at start_location."
+  @doc "Create a player; board_location starts at corner."
   @spec new(String.t(), non_neg_integer(), coord(), DateTime.t()) :: Player.t()
-  def new(name, color, {x, y} = start_location, joined_at)
+  def new(name, color, {x, y} = corner_coord, joined_at)
       when is_binary(name) and is_integer(x) and is_integer(y) do
     %Player{
       id: :crypto.strong_rand_bytes(8) |> Base.url_encode64(padding: false),
       name: name,
       color: color,
-      start_location: start_location,
-      board_location: start_location,
+      corner: corner_coord,
+      board_location: corner_coord,
       joined_at: joined_at
     }
   end
@@ -52,6 +52,19 @@ defmodule Player do
   def update_board_location(%Player{} = player, {x, y})
       when is_integer(x) and is_integer(y) and x >= 0 and y >= 0 do
     %Player{player | board_location: {x, y}}
+  end
+
+  @doc "Rename user to a new name."
+  @spec rename(t(), String.t()) :: t()
+  def rename(%Player{} = player, new_name) do
+    %Player{player | name: new_name}
+  end
+
+  @doc "Update starting corner location on board."
+  @spec update_corner(t(), coord()) :: t()
+  def update_corner(%Player{} = player, {x, y})
+      when is_integer(x) and is_integer(y) and x >= 0 and y >= 0 do
+    %Player{player | corner: {x, y}}
   end
 
   @doc "Set current piece (use nil to clear)."
@@ -83,13 +96,13 @@ defmodule Player do
   @spec used_first_piece?(t()) :: boolean()
   def used_first_piece?(%Player{points: points}), do: points > 0
 
-  @doc "Reset for new game; keep name/start_location."
+  @doc "Reset for new game; keep name/corner."
   @spec reset_for_new_game(t()) :: t()
   def reset_for_new_game(%Player{} = player) do
     %Player{
       player
       | points: 0,
-        board_location: player.start_location,
+        board_location: player.corner,
         current_piece: nil
     }
   end
